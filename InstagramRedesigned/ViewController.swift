f61eb60fa846bedc57af70b7769e40d4e4a94ed9
kostatyy <kostatyy.freelance@mail.ru>
2021-07-07 15:08:54 +0300

    Init Commit

---
+++ InstagramRedesigned/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramRedesigned
+//
+//  Created by Macbook Pro on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
